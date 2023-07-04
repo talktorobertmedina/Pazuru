cfc2cf27b31ffb0e33d5bc3ba7d892b4291d1b5d
Robert Medina <talktorobertmedina@gmail.com>
2023-07-04 11:08:04 -0400

    first draft of listview in widget library pane

--- src/app/widgetlibrary/WidgetLibraryController.java
+++ src/app/widgetlibrary/WidgetLibraryController.java
@@ -4,11 +4,18 @@ import java.net.URL;
 
 import app.BaseController;
 import app.FxmlEnumList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import screenbuilder.widgets.base.Component;
+import screenbuilder.widgets.container.Group;
+import screenbuilder.widgets.controls.AnalogReadout;
+import screenbuilder.widgets.controls.DigitalReadout;
+import screenbuilder.widgets.controls.Label;
+import screenbuilder.widgets.shape.Rectangle;
 
 public class WidgetLibraryController extends BaseController {
 
@@ -27,6 +34,11 @@ public class WidgetLibraryController extends BaseController {
 	@FXML
 	ListView<Component> shapesListView;
 	
+	// TODO: optional: move these components into a widgetlibrary type loading class
+	ObservableList<Component> containersList = FXCollections.observableArrayList(new Group());
+	ObservableList<Component> controlsList = FXCollections.observableArrayList(new Label(), new DigitalReadout(), new AnalogReadout());
+	ObservableList<Component> shapesList = FXCollections.observableArrayList(new Rectangle());
+	
 	public WidgetLibraryController() {
 		this( WidgetLibraryController.class.getResource(FxmlEnumList.WIDGETLIBRARY_FXML));
 	}
@@ -36,6 +48,10 @@ public class WidgetLibraryController extends BaseController {
 
 	@FXML
 	public void initialize() {
-
+		// TODO: add custom cell factory to display proper name of component
+		// TODO: add drag and drop feature inside each cell
+		containersListView.setItems(containersList);
+		controlsListView.setItems(controlsList);
+		shapesListView.setItems(shapesList);
 	}
 }
