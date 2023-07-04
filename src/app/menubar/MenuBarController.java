f8732651107b3ee6b05290615f88377680d91e3b
Robert Medina <talktorobertmedina@gmail.com>
2023-07-03 22:54:50 -0400

    add handle for open and save as menu items

--- src/app/menubar/MenuBarController.java
+++ src/app/menubar/MenuBarController.java
@@ -1,13 +1,17 @@
 package app.menubar;
 
+import java.io.File;
 import java.net.URL;
 
+import app.AppUtils;
 import app.BaseController;
 import app.FxmlEnumList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
 
 public class MenuBarController extends BaseController {
 
@@ -32,4 +36,28 @@ public class MenuBarController extends BaseController {
 	private MenuItem fileMenuSaveAsItem;
 	@FXML
 	private MenuItem fileMenuExitItem;
+
+	@FXML
+	private void handleSaveAsFile(ActionEvent e) {
+		String lastNewFilePath = AppUtils.getAppProperties().getProperty("LAST_NEW_FILE_PATH");
+		if (lastNewFilePath.isEmpty()) {
+			lastNewFilePath = System.getProperty("user.dir");
+		}
+		
+		FileChooser fc = new FileChooser();
+		fc.setInitialDirectory(new File(lastNewFilePath));
+		File chosenFile = fc.showSaveDialog(null);
+	}
+
+	@FXML
+	private void handleOpenFile(ActionEvent e) {
+		String lastNewFilePath = AppUtils.getAppProperties().getProperty("LAST_NEW_FILE_PATH");
+		if (lastNewFilePath.isEmpty()) {
+			lastNewFilePath = System.getProperty("user.dir");
+		}
+		
+		FileChooser fc = new FileChooser();
+		fc.setInitialDirectory(new File(lastNewFilePath));
+		File chosenFile = fc.showOpenDialog(null);
+	}
 }
--- src/app/task/Task.java
+++ src/app/task/Task.java
@@ -3,10 +3,10 @@ package app.task;
 /**
  * Class for implementing a task. This task is used by task manager to execute tasks.
  */
-public abstract class Task {
+public interface Task {
 	
-	public abstract boolean canDo();
-	public abstract void action();
-	public abstract void undo();
-	public abstract void redo();
+	public boolean canDo();
+	public void action();
+	public void undo();
+	public void redo();
 }
